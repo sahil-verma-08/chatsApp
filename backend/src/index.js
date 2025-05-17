d6e1583e0b5cb1be1e5d79334aeceaed3b44e6f5
sahil-verma-08 <sahilverma2431@gmail.com>
2025-05-17 18:08:13 +0530

    project-completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,8 +13,8 @@ import { fileURLToPath } from "url";
 dotenv.config();
 const PORT=process.env.PORT || 9000
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const __dirname = path.resolve()
 
 app.use(cookieParser());
 
@@ -24,18 +24,17 @@ app.use(
       credentials: true,
     })
   );
-
-  app.use(express.json({ limit: '10mb' }));
-  app.use(express.urlencoded({ limit: '10mb', extended: true }));
+ app.use(express.json({ limit: '10mb' }));
+ app.use(express.urlencoded({ limit: '10mb', extended: true }));
   
 
 app.use ("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes);
 
 if (process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*",(req,res)=>{
+  app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
   })
 }
